11e3f59a9968266ded41ca5659761948e07b42ba
Aleksandr Hilinichenko <sasha-hi@spfmail.org>
2014-01-11 11:23:30 +0200

    Add копирование пакета документов в другую организацию

---
+++ Pers_uchet_org/Forms/CopyPacketOtherOrgForm.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Pers_uchet_org
+{
+    partial class CopyPacketOtherOrgForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.orgsComboBox = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.copyButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(243, 26);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Выберите организацию, в которую \r\nбудет скопирован пакет документов СЗВ-1 № ";
+            // 
+            // orgsComboBox
+            // 
+            this.orgsComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.orgsComboBox.FormattingEnabled = true;
+            this.orgsComboBox.Location = new System.Drawing.Point(12, 38);
+            this.orgsComboBox.Name = "orgsComboBox";
+            this.orgsComboBox.Size = new System.Drawing.Size(321, 21);
+            this.orgsComboBox.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.Firebrick;
+            this.label3.Location = new System.Drawing.Point(119, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Внимание !!!";
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(12, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(322, 67);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Копирование пакета документов СЗВ-1 из одной организации в другую возможно только" +
+    " в рамках одного и того же отчетного года!\r\n";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(12, 149);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(322, 85);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "При копировании пакета, всем работникам, по которым имеются документы СЗВ-1 в дан" +
+    "ном пакете, будет добавлена принадлежность к новой организации (в которую будет " +
+    "скопирован пакет)!";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Location = new System.Drawing.Point(258, 243);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 8;
+            this.cancelButton.Text = "Отмена";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // copyButton
+            // 
+            this.copyButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.copyButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.copyButton.Location = new System.Drawing.Point(156, 243);
+            this.copyButton.Name = "copyButton";
+            this.copyButton.Size = new System.Drawing.Size(96, 23);
+            this.copyButton.TabIndex = 9;
+            this.copyButton.Text = "Копировать";
+            this.copyButton.UseVisualStyleBackColor = true;
+            this.copyButton.Click += new System.EventHandler(this.copyButton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(12, 310);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(326, 177);
+            this.panel1.TabIndex = 10;
+            // 
+            // CopyPacketOtherOrgForm
+            // 
+            this.AcceptButton = this.copyButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.cancelButton;
+            this.ClientSize = new System.Drawing.Size(345, 278);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.copyButton);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.orgsComboBox);
+            this.Controls.Add(this.label2);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CopyPacketOtherOrgForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Копирование пакета в другую организацию";
+            this.Load += new System.EventHandler(this.CopyPacketForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox orgsComboBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button copyButton;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ Pers_uchet_org/Forms/CopyPacketOtherOrgForm.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace Pers_uchet_org
+{
+    public partial class CopyPacketOtherOrgForm : Form
+    {
+        #region Поля
+        const string viewCol = "view_col";
+        private string _connection;
+        private long listId;
+        private Operator _operator;
+        private DataTable _orgTable;
+        private BindingSource _orgBS;
+        
+        
+        #endregion
+
+        public CopyPacketOtherOrgForm()
+        {
+            InitializeComponent();
+        }
+
+        public CopyPacketOtherOrgForm(Operator _operator, string _connection, long listId)
+            : this()
+        {
+            this._operator = _operator;
+            this._connection = _connection;
+            this.listId = listId;
+        }
+
+        private void CopyPacketForm_Load(object sender, EventArgs e)
+        {
+            label2.Text += listId;
+            _orgTable = Org.CreateTable();
+            _orgTable.Columns.Add(viewCol);
+            _orgBS = new BindingSource();
+            _orgBS.DataSource = _orgTable;
+
+            orgsComboBox.DataSource = _orgBS;
+            orgsComboBox.ValueMember = Org.id;
+            orgsComboBox.DisplayMember = viewCol;
+
+            if (_operator == null)
+            {
+                MainForm.ShowErrorMessage("Пользователь не найден", "Ошибка");
+                this.Close();
+                return;
+            }
+
+            string selectText;
+            if (_operator.candeleteVal == 0)
+            {
+                selectText = Org.GetSelectCommandText();
+            }
+            else
+            {
+                selectText = Org.GetSelectTextByOperator(_operator.idVal);
+            }
+
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(selectText, _connection);
+            _orgTable.Rows.Clear();
+
+            adapter.Fill(_orgTable);
+            foreach (DataRow rowItem in _orgTable.Rows)
+            {
+                rowItem[viewCol] = string.Format("{0}    {1}", rowItem[Org.regnum], rowItem[Org.name]);
+            }
+            _orgTable.AcceptChanges();
+            if (_orgBS.Count < 1)
+                copyButton.Enabled = false;
+            //TODO: this.SetPrivilege();
+        }
+
+        private void copyButton_Click(object sender, EventArgs e)
+        {
+            StajDohodForm.NewOrgId = (long)orgsComboBox.SelectedValue;
+        }
+    }
+}
